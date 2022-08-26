dc506f4d27a954cb9fe37160bb0d868db5b99b86
AshikurRahmanMunna <ashikurrahmanmunna3@gmail.com>
2022-08-26 10:20:21 +0600

    raw node Requset response

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
-const http = require('http');
+const http = require("http");
+const url = require("url");
 
-const server = http.createServer(function(req, res) {
-    res.sendDate('Hello')
-})
-
+const server = http.createServer(function (req, res) {
+  switch (req.url) {
+    case "/":
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.write(JSON.stringify({ course: "ACC" }));
+      res.end();
+      break;
+    case "/contact":
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write("<h1>Contact page</h1>");
+      res.end();
+      break;
+  }
+});
+console.log(url);
 const PORT = 5000;
 server.listen(5000);
 console.log(`server is running at ${PORT}`);
