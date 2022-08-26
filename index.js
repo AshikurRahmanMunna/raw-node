1a6518f07b675dc878f14095ab6b74c4a09b2294
AshikurRahmanMunna <ashikurrahmanmunna3@gmail.com>
2022-08-26 13:51:24 +0600

    Explored EventEmitter

--- index.js
+++ index.js
@@ -1,35 +1,11 @@
-const http = require("http");
-const url = require("url");
-const fs = require("fs");
+const events = require('events');
 
-const server = http.createServer(function (req, res) {
-  if(req.url = '/') {
-    // fs.readFile('data.ai', (error, data) => {
-    //     if(error) {
-    //         res.write('Failed to Read Data !!!!!!');
-    //         res.end();
-    //     }
-    //     else {
-    //         res.write(data);
-    //         res.end();
-    //     }
-    // })
-    // const data = fs.readFileSync('data.ai');
-    // res.write(data);
-    // res.end();
+const eventEmitter = new events.EventEmitter();
 
-    fs.rename('newData.txt', 'oldData.txt', (err) => {
-        if(err) {
-            res.write('Failed to rename file');
-            res.end();
-        } else {
-            res.write('Successfully renamed file');
-            res.end();
-        }
-    })
-  }
-});
+const chitkarDibo = () => {
+    console.log('Oi beta');
+}
 
-const PORT = 5000;
-server.listen(5000);
-console.log(`server is running at ${PORT}`);
+eventEmitter.on('scream', chitkarDibo).chitkarDibo
+
+eventEmitter.emit('scream');
